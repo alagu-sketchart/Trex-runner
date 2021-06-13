bb2a814bc918a49fbe4c636f149fb5a16b145563
alagu-sketchart <74680709+alagu-sketchart@users.noreply.github.com>
2021-06-13 04:11:32 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,11 +2,12 @@ var PLAY = 1;
 var END = 0;
 var gameState = PLAY;
 
-var trex, trex_running, trex_collided;
+var ninja, ninja_running, ninja_dead, ninja_jump;
 var ground, invisibleGround, groundImage;
 
 var cloudsGroup, cloudImage, backgroundImg;
 var obstaclesGroup, obstacle1, obstacle2;
+
 var jumpSound, collidedSound;
 var score=0;
 
@@ -21,8 +22,9 @@ function preload(){
   backgroundImg = loadImage("Background.png");
   sunAnimation = loadAnimation("Scary_Sun.png", "Scary_Sun.png", "Scary_Sun.png", "Scary_Sun.png", "Scary_Sun.png", "Purple_Sun.png");
   
-  trex_running = loadAnimation("Dino_run_1.png", "Dino_run_2.png", "Dino_run_3.png");
-  trex_collided = loadAnimation("Trex_Dead.png");
+  ninja_running = loadAnimation("ninja_1.png", "ninja_2.png");
+  ninja_dead = loadAnimation("dead_.png");
+  ninja_jump = loadAnimation("ninja_jump.png");
   
   groundImage = loadImage("Ground.png");
   
@@ -42,20 +44,22 @@ function setup() {
   sun = createSprite(width-75,height-350,10,10);
   sun.addAnimation("sun", sunAnimation);
   sun.scale = 0.4;
-  
-  trex = createSprite(50,height-70,20,50);
-  trex.addAnimation("running", trex_running);
-  trex.addAnimation("collided", trex_collided);
-  trex.setCollider('circle',0,0,175);
-  trex.scale = 0.15;
-  //trex.debug=true
-  
-  invisibleGround = createSprite(width/2,height-10,width,125);  
+   
+  invisibleGround = createSprite(width/2,height+10,width,125);  
   ground = createSprite(width/2, 1000 ,width, 2);
   ground.addImage("ground",groundImage);
   ground.scale = 4;
   ground.x = width/2;
-  ground.velocityX = -(6 + 3*score/100);
+  ground.velocityX = +(6 + 3*score/100);
+  invisibleGround.debug = true
+
+  ninja = createSprite(50,height-90,20,50);
+  ninja.addAnimation("running", ninja_running);
+  ninja.addAnimation("collided", ninja_dead);
+  ninja.addAnimation("jump", ninja_jump);
+  ninja.setCollider('circle',0,50,150);
+  ninja.scale = 0.15;
+  //ninja.debug=true
   
   gameOver = createSprite(width/2,height/2- 50);
   gameOver.addImage(gameOverImg);
@@ -79,57 +83,61 @@ function draw() {
   background(backgroundImg);
   textSize(20);
   fill("black")
-  text("Score: "+ score,30,550);
+  text("Score: "+ score,width/2,550);
   
   if (gameState===PLAY){
     score = score + Math.round(getFrameRate()/60);
     ground.velocityX = -(6 + 3*score/100);
-    trex.velocityX = 2;
-    camera.position.x = displayWidth/2;
-    camera.position.y = trex.y;
+    ninja.velocityX = 2;
+    camera.position.x = ninja.x;
+    camera.position.y = ninja.y;
 
-    if((keyDown("SPACE")) && trex.y  >= height-120) {
+    if((keyDown("SPACE")) && ninja.y  >= height-90) {
       jumpSound.play();
-      trex.velocityY = -10;
+      ninja.velocityY = -10;
+      ninja.changeAnimation("jump", ninja_jump)
+    }else {
+      ninja.changeAnimation("running",ninja_running);
     }
+
     if(score>0 && score%100 == 0) {
         checkPointSound.play();
     }
-    trex.velocityY = trex.velocityY + 0.8
+    ninja.velocityY = ninja.velocityY + 0.8
     if (ground.x < 0){
-      ground.x = ground.width/2;
+      ground.x = ground.width;
     }
   
-    trex.collide(invisibleGround);
+    ninja.collide(invisibleGround);
     spawnClouds();
     spawnObstacles();
   
-    if(obstaclesGroup.isTouching(trex)){
+    if(obstaclesGroup.isTouching(ninja)){
         collidedSound.play()
         gameState = END;
     }
   }
-  else if (gameState === END) {
-    gameOver.visible = true;
-    restart.visible = true;
-    
-    ground.velocityX = 0;
-    trex.velocityY = 0;
-    trex.velocityX = 0;
-    obstaclesGroup.setVelocityXEach(0);
-    obstaclesGroup.setLifetimeEach(-1);
-    cloudsGroup.setVelocityXEach(0);
-    cloudsGroup.setLifetimeEach(-1);
-    trex.changeAnimation("collided",trex_collided);
-    
-    fill("Gold");
-    text("Click on Restart to Play Again!",width/2.5,height-325);
-    
-    fill(125,25,0);
-    text("RIP Mr.Rex",width/2.15,height-150);
-    
-    if(mousePressedOver(restart)) {      
-      reset();
+    else if (gameState === END) {
+        gameOver.visible = true;
+        restart.visible = true;
+        
+        ground.velocityX = 0;
+        ninja.velocityY = 0;
+        ninja.velocityX = 0;
+        obstaclesGroup.setVelocityXEach(0);
+        obstaclesGroup.setLifetimeEach(-1);
+        cloudsGroup.setVelocityXEach(0);
+        cloudsGroup.setLifetimeEach(-1);
+        ninja.changeAnimation("collided",ninja_dead);
+        
+        fill("Gold");
+        text("Click on Restart to Play Again!", width/2 ,height-325);
+        
+        fill(125,25,0);
+        text("RIP Master Ninja", ninja.x, height-150);
+        
+        if(mousePressedOver(restart)) {      
+          reset();
     }}
   drawSprites();
 }
@@ -143,8 +151,8 @@ function spawnClouds() {
     cloud.velocityX = -3;
     
     cloud.lifetime = 900;
-    cloud.depth = trex.depth;
-    trex.depth = trex.depth+1;
+    cloud.depth = ninja.depth;
+    ninja.depth = ninja.depth+1;
     cloudsGroup.add(cloud);
   }}
 
@@ -166,21 +174,21 @@ function spawnObstacles() {
     
     obstacle.scale = 1;
     obstacle.lifetime = 300;
-    obstacle.depth = trex.depth;
-    trex.depth +=1;
+    obstacle.depth = ninja.depth;
+    ninja.depth +=1;
     obstaclesGroup.add(obstacle);
   }
 }
 function reset(){
-  gameState = PLAY;
-  gameOver.visible = false;
-  restart.visible = false;
-  
-  obstaclesGroup.destroyEach();
-  cloudsGroup.destroyEach();
-  
-  trex.changeAnimation("running",trex_running);
-  trex.x = 20;
-  
-  score = 0;
+    gameState = PLAY;
+    gameOver.visible = false;
+    restart.visible = false;
+    
+    obstaclesGroup.destroyEach();
+    cloudsGroup.destroyEach();
+    
+    ninja.changeAnimation("running",ninja_running);
+    ninja.x = 20;
+    
+    score = 0;
 }
